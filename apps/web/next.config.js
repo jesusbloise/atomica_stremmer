a96c814a85ed221c07597f606ac0700771e6ea2c
jesus bloise <jesusbloise@gmail.com>
2026-01-06 12:56:54 -0300

    resolviendo los next.config duplicados dejamos solo uno con una estructura completa

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,28 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-}
-module.exports = nextConfig
+  // ⚠️ Mientras limpiamos types y lint
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  // Variables de entorno públicas en runtime
+  env: {
+    HIGHNOTE_SK_LIVE_KEY: process.env.HIGHNOTE_SK_LIVE_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+  },
+};
+
+module.exports = nextConfig;
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   typescript: { ignoreBuildErrors: true },
+//   eslint: { ignoreDuringBuilds: true },
+// }
+// module.exports = nextConfig
 
--- apps/web/next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    HIGHNOTE_SK_LIVE_KEY: process.env.HIGHNOTE_SK_LIVE_KEY,
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-  },
-};
-
-export default nextConfig;
